3e6ce89e5b630c089954560d7324e78a5381e009
limseongkyu <96x60812@naver.com>
2020-04-21 23:47:50 +0900

    completed_code

--- 백준_수학1/달팽이는올라가고싶다2.py
+++ 백준_수학1/달팽이는올라가고싶다2.py
@@ -1,14 +1,5 @@
 up, down, height = input().split()
 
-afternoon_distance = 0
-night_distance = 0
-day = 1
+import math
 
-
-while afternoon_distance < int(height) :
-
-    afternoon_distance = night_distance + int(up)
-    night_distance = afternoon_distance - int(down)
-    day = day + 1
-
-print(day)
+a, b, v = map(int, sys.stdin.readline().split())
